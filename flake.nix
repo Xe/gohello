9357b612baada85fcb03abae251a6a8eeb4ed45f
Xe <me@christine.website>
2022-02-21 13:21:20 +0000

    add defaultApp
    Signed-off-by: Xe <me@christine.website>

--- flake.nix
+++ flake.nix
@@ -11,7 +11,8 @@
       version = builtins.substring 0 8 self.lastModifiedDate;
 
       # System types to support.
-      supportedSystems = [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
+      supportedSystems =
+        [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
 
       # Helper function to generate an attrset '{ x86_64-linux = f "x86_64-linux"; ... }'.
       forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
@@ -19,15 +20,12 @@
       # Nixpkgs instantiated for supported system types.
       nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; });
 
-    in
-    {
+    in {
 
       # Provide some binary packages for selected system types.
       packages = forAllSystems (system:
-        let
-          pkgs = nixpkgsFor.${system};
-        in
-        {
+        let pkgs = nixpkgsFor.${system};
+        in {
           go-hello = pkgs.buildGoModule {
             pname = "go-hello";
             inherit version;
@@ -45,7 +43,8 @@
             # remeber to bump this hash when your dependencies change.
             #vendorSha256 = pkgs.lib.fakeSha256;
 
-            vendorSha256 = "sha256-pQpattmS9VmO3ZIQUFn66az8GSmB4IvYhTTCFn6SUmo=";
+            vendorSha256 =
+              "sha256-pQpattmS9VmO3ZIQUFn66az8GSmB4IvYhTTCFn6SUmo=";
           };
         });
 
@@ -53,5 +52,10 @@
       # flake provides only one package or there is a clear "main"
       # package.
       defaultPackage = forAllSystems (system: self.packages.${system}.go-hello);
+
+      defaultApp = forAllSystems (system: {
+        type = "app";
+        program = "${self.packages.${system}.go-hello}/bin/go-hello";
+      });
     };
 }
